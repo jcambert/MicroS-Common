63d575e4fac712d418ad80f5d66f53a764263600
Ambert Jean-Christophe <jc.ambert@free.fr>
2019-08-26 16:52:50 +0200

    Add RestEase Support

---
+++ src/MicroS-Common/RestEase/RestEaseServiceNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MicroS_Common.RestEase
+{
+    public class RestEaseServiceNotFoundException : Exception
+    {
+        public string ServiceName { get; set; }
+
+        public RestEaseServiceNotFoundException(string serviceName) : this(string.Empty, serviceName)
+        {
+        }
+
+        public RestEaseServiceNotFoundException(string message, string serviceName) : base(message)
+        {
+            ServiceName = serviceName;
+        }
+    }
+}
