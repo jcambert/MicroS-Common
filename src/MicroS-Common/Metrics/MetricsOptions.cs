0babd41da48521cf6f319b69d114800672901577
Ambert Jean-Christophe <jc.ambert@free.fr>
2019-08-26 13:57:09 +0200

    Add Metrics Support

---
+++ src/MicroS-Common/Metrics/MetricsOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroS_Common.Metrics
+{
+    public class MetricsOptions
+    {
+        public bool Enabled { get; set; }
+        public bool InfluxEnabled { get; set; }
+        public bool PrometheusEnabled { get; set; }
+        public string PrometheusFormatter { get; set; }
+        public string InfluxUrl { get; set; }
+        public string Database { get; set; }
+        public int Interval { get; set; }
+        public IDictionary<string, string> Tags { get; set; }
+    }
+}
